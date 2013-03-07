9bb7cdcfd0f7a2175fd5c2a15774775eb3fcd5e4
Majiir Paktu <majiir@nabaal.net>
2013-03-06 21:43:32 -0500

    Added map viewer module

---
+++ Plugin/KethaneMapViewer.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Kethane
+{
+    public class KethaneMapViewer : PartModule
+    {
+        [KSPEvent(guiActive = true, guiName = "Show Map", active = true)]
+        public void ShowMap()
+        {
+            KethaneController.GetInstance(this.vessel).ShowDetectorWindow = true;
+        }
+
+        [KSPEvent(guiActive = true, guiName = "Hide Map", active = false)]
+        public void HideMap()
+        {
+            KethaneController.GetInstance(this.vessel).ShowDetectorWindow = false;
+        }
+
+        public override void OnUpdate()
+        {
+            var controller = KethaneController.GetInstance(this.vessel);
+            Events["ShowMap"].active = !controller.ShowDetectorWindow;
+            Events["HideMap"].active = controller.ShowDetectorWindow;
+        }
+    }
+}
